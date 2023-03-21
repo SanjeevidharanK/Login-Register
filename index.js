58ca02a2c4a8edeba76742059f0600eff2f236e3
SanjeevidharanK <113497192+SanjeevidharanK@users.noreply.github.com>
2023-03-21 23:24:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const loginposition = document.getElementById('#no1')
+// const registposition = document.getElementById('#no2')
+
+// console.log(loginposition)
+console.log(1)
+// document.querySelector()
