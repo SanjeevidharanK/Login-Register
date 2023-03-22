0986de0eccb69f4684a4e1e1ee162818dc59777c
SanjeevidharanK <113497192+SanjeevidharanK@users.noreply.github.com>
2023-03-22 10:05:17 +0530

    second commit

--- index.js
+++ index.js
@@ -1,6 +1,68 @@
-const loginposition = document.getElementById('#no1')
-// const registposition = document.getElementById('#no2')
+const Login = document.getElementById('no1');
+const register = document.getElementById('no2');
 
-// console.log(loginposition)
-console.log(1)
-// document.querySelector()
+const log_btn = document.getElementById('lo-btn');
+const reg_btn = document.getElementById('re-btn');
+const close = document.getElementById('clo');
+const cl = document.getElementById('clos');
+
+const regis = document.getElementById('regi-btn')
+
+
+
+log_btn.onclick = function(){
+    Login.style.display="block";
+    register.style.display="block"
+    Login.style.position = "absolute";
+    Login.style.zIndex = "-1";
+
+    register.style.position ="relative";
+    register.style.zIndex = "1";
+}
+reg_btn.onclick = function(){
+    Login.style.display="block";
+    register.style.display="block";
+    register.style.position = "absolute";
+    register.style.zIndex = "-1";
+    Login.style.transition="transform .5s ease";
+
+    Login.style.position ="relative";
+    Login.style.zIndex = "1";
+}
+regis.onclick = function(){
+    Login.style.display="block";
+    register.style.display="block"
+    register.style.position = "absolute";
+    register.style.zIndex = "-1";
+
+    Login.style.position ="relative";
+    Login.style.zIndex = "1";
+}
+close.onclick = function(){
+    Login.style.display="none";
+    register.style.display="none";
+
+
+}
+cl.onclick = function(){
+    Login.style.display="none";
+    register.style.display="none";
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
